ddd89e0eaa1ba05d4c87dfd98b42f5b395dcb0e8
SofiPolak <sofi_polak@hotmail.com>
2024-05-26 12:05:16 -0300

    Cambio en ruta script

--- segundaPreEntrega/src/public/js/products.js
+++ segundaPreEntrega/src/public/js/products.js
@@ -13,8 +13,10 @@ btnSend.addEventListener('click', () => {
 function addToCart() {
     document.querySelectorAll('.agregar-carrito').forEach(button => {
         button.addEventListener('click', async (event) => {
-            const productId = button.getAttribute('data-product-id');
-            const cartId = document.getElementById('carritoId').value;
+            //const productId = button.getAttribute('data-product-id');
+            //const cartId = document.getElementById('carritoId').value;
+            const productId = "664e76aea1604d347baf79ed";
+            const cartId = "664e76b4a1604d347baf79ef";
   
             try {
                 const response = await fetch(`/api/carts/${cartId}/products/${productId}`, {
